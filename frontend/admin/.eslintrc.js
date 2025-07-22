dac3c500a8fb86691fc9db7c9724d6424ad4141c
ChoByeongJun-PNT <cbj@pandt.io>
2025-07-22 18:59:50 +0900

    front: admin build 완료 version
    1. "package.json" directory로 이동 (terminal)
    
    2.  "cross-env" 적용
    "npm install" or "npm install --save-dev cross-env --legacy-peer-deps"
    
    3. build
    "npm run build"
    
    4. build된 version 실행
    "npm run start"
    
    5. localhost:3000 접근 -> 로그인 창 나오면 성공

--- frontend/admin/.eslintrc.js
+++ frontend/admin/.eslintrc.js
@@ -22,9 +22,7 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
+    'prettier'
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
